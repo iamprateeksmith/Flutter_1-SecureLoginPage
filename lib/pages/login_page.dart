51ad901d899c690f9279859ee883d79ce39d8987
iamprateeksmith <70465722+iamprateeksmith@users.noreply.github.com>
2021-05-31 20:42:21 +0530

    Third Commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,17 +6,20 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-          textScaleFactor: 2.0,
-        ),
+      child: Column(
+        children: [Image.asset("assets/images/secure_login_1.png")],
       ),
+      //child: Center(
+      //  child: Text(
+      //    "Login Page",
+      //    style: TextStyle(
+      //      fontSize: 40,
+      //      color: Colors.blue,
+      //      fontWeight: FontWeight.bold,
+      //    ),
+      //    textScaleFactor: 2.0,
+      //  ),
+      // ),
     );
   }
 }
